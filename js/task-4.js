d9d8d6dd3daf65453d51acb5394d68422f127d36
ArtemijRud <112616679+ArtemijRud@users.noreply.github.com>
2023-12-17 18:32:00 +0200

    17.12

--- js/task-4.js
+++ js/task-4.js
@@ -16,6 +16,26 @@
 
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
 
+function getShippingCost(country) {
+  let price; //init price
+  switch (country) {
+    case "China":
+      price = 100;
+      return `Shipping to ${country} will cost ${price} credits`;
+    case "Chile":
+      price = 250;
+      return `Shipping to ${country} will cost ${price} credits`;
+    case "Australia":
+      price = 170;
+      return `Shipping to ${country} will cost ${price} credits`;
+    case "Jamaica":
+      price = 120;
+      return `Shipping to ${country} will cost ${price} credits`;
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
+}
+
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
 console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
